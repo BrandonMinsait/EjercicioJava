7b9741e1836966e49cca52a5ad9471c3439b6428
brandonalejandro.pacheco.contractor <brandonalejandro.pacheco.contractor@bbva.com>
2022-11-16 20:00:49 -0600

    Version final

--- src/ejercicio/Nip.java
+++ src/ejercicio/Nip.java
@@ -2,7 +2,7 @@ package ejercicio;
 
 public class Nip {
     
-    protected static int nip = 1234;
+    protected static int nip = 1235;
     
     public boolean verificarNIP(int pin){
         if(pin == nip){
